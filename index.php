a96fc48857f291ef143c494949e48fdabe6d85f4
mtallur567 <64742324+mtallur567@users.noreply.github.com>
2021-03-01 20:19:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <header><title>This is title</title></header>
 <body>
-<?php print "this is version2 of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n"; ?>
+<?php print "I am running on host IP Address ->" . " (" . $_SERVER['SERVER_ADDR'] . ")" . "\n"; ?>
 </body>
 </html>
