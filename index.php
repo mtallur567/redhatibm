eef46ffd029af69988fb8e669b8210af9db499c7
MAHESHBABU Babu <tallbabu@in.ibm.com>
2021-03-01 15:15:48 +0530

    Updated app to version 1

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <header><title>This is title</title></header>
 <body>
-<?php print "this is version of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n"; ?>
+<?php print "this is version1 of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n"; ?>
 </body>
 </html>
